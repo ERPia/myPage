48f5145a5fee2121c0a78341ca2728635daa8694
superEggSong <gyaradosz@naver.com>
2015-11-13 22:14:29 +0900

    daa

--- www/js/app.js
+++ www/js/app.js
@@ -6,16 +6,16 @@
 angular.module('starter', ['ionic','ionic.service.core','ionic.service.push', 'starter.controllers', 'tabSlideBox' ,'ngCordova'
 	, 'starter.services', 'chart.js'])
 
-.constant('ERPiaAPI',{
-	url:'http://localhost:8100/include'
-	, toast:'N'
-})
+// .constant('ERPiaAPI',{
+// 	url:'http://localhost:8100/include'
+// 	, toast:'N'
+// })
 
 // 실제 사용시
-// .constant('ERPiaAPI',{
-// 	url:'http://www.erpia.net/include'
-// 	, toast:'Y'
-// })
+.constant('ERPiaAPI',{
+	url:'http://www.erpia.net/include'
+	, toast:'Y'
+})
 
 .run(function($ionicPlatform, $ionicPush, $ionicUser, $rootScope) {
 	$ionicPlatform.ready(function() {
