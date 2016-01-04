c843f21f0aece449ee5983fe1102114903a10891
superEggSong <kaming31@naver.com>
2016-01-04 14:35:23 +0900

    comitkm

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -3,37 +3,15 @@ module.exports = [
     {
         "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
         "id": "com.phonegap.plugins.PushPlugin.PushNotification",
+        "pluginId": "com.phonegap.plugins.PushPlugin",
         "clobbers": [
             "PushNotification"
         ]
     },
     {
-        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
-        "id": "cordova-plugin-whitelist.whitelist",
-        "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-x-toast/www/Toast.js",
-        "id": "cordova-plugin-x-toast.Toast",
-        "clobbers": [
-            "window.plugins.toast"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-x-toast/test/tests.js",
-        "id": "cordova-plugin-x-toast.tests"
-    },
-    {
-        "file": "plugins/de.appplant.cordova.plugin.printer/www/printer.js",
-        "id": "de.appplant.cordova.plugin.printer.Printer",
-        "clobbers": [
-            "plugin.printer",
-            "cordova.plugins.printer"
-        ]
-    },
-    {
         "file": "plugins/org.apache.cordova.device/www/device.js",
         "id": "org.apache.cordova.device.device",
+        "pluginId": "org.apache.cordova.device",
         "clobbers": [
             "device"
         ]
@@ -41,16 +19,10 @@ module.exports = [
     {
         "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
         "id": "org.apache.cordova.vibration.notification",
+        "pluginId": "org.apache.cordova.vibration",
         "merges": [
             "navigator.notification",
             "navigator"
-        ]
-    },
-    {
-        "file": "plugins/com.gnustory.kakaolink/www/js/KakaoLinkPlugin.js",
-        "id": "com.gnustory.kakaolink.KakaoLinkPlugin",
-        "clobbers": [
-            "KakaoLinkPlugin"
         ]
     }
 ];
@@ -58,12 +30,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "com.phonegap.plugins.PushPlugin": "2.5.0",
-    "cordova-plugin-whitelist": "1.1.1-dev",
-    "cordova-plugin-x-toast": "2.2.1",
-    "de.appplant.cordova.plugin.printer": "0.7.1",
     "org.apache.cordova.device": "0.3.0",
-    "org.apache.cordova.vibration": "0.3.13",
-    "com.gnustory.kakaolink": "0.0.1"
+    "org.apache.cordova.vibration": "0.3.13"
 }
 // BOTTOM OF METADATA
 });
