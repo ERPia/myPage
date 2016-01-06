7322003868b4a24498f6db734a21db190528b8f5
superEggSong <gyaradosz@naver.com>
2016-01-06 11:12:42 +0900

    add

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,11 +1,47 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/com.gnustory.kakaolink/www/js/KakaoLinkPlugin.js",
+        "id": "com.gnustory.kakaolink.KakaoLinkPlugin",
+        "pluginId": "com.gnustory.kakaolink",
+        "clobbers": [
+            "KakaoLinkPlugin"
+        ]
+    },
+    {
         "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
         "id": "com.phonegap.plugins.PushPlugin.PushNotification",
         "pluginId": "com.phonegap.plugins.PushPlugin",
         "clobbers": [
             "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-x-toast/www/Toast.js",
+        "id": "cordova-plugin-x-toast.Toast",
+        "pluginId": "cordova-plugin-x-toast",
+        "clobbers": [
+            "window.plugins.toast"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-x-toast/test/tests.js",
+        "id": "cordova-plugin-x-toast.tests",
+        "pluginId": "cordova-plugin-x-toast"
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.printer/www/printer.js",
+        "id": "de.appplant.cordova.plugin.printer.Printer",
+        "pluginId": "de.appplant.cordova.plugin.printer",
+        "clobbers": [
+            "plugin.printer",
+            "cordova.plugins.printer"
         ]
     },
     {
@@ -29,7 +65,11 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "com.gnustory.kakaolink": "0.0.1",
     "com.phonegap.plugins.PushPlugin": "2.5.0",
+    "cordova-plugin-whitelist": "1.1.1-dev",
+    "cordova-plugin-x-toast": "2.2.1",
+    "de.appplant.cordova.plugin.printer": "0.7.1",
     "org.apache.cordova.device": "0.3.0",
     "org.apache.cordova.vibration": "0.3.13"
 }
